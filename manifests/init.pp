d10902c9b773a810bc3b6e796bf01c32a6759f39
Lauri Helenius <a1402989@myy.haaga-helia.fi>
2016-10-31 14:01:56 -0700

    init & template

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+class apache2-index {
+package { apache2:
+ensure => “installed”,
+}
+file { “/var/www/html/index.html”:
+content => template(“apache2-index/templates/index.html”),
+}
+service { “apache2”:
+ensure => “running”,
+}
+}
