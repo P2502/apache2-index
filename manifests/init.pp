1d08072ec6c2f90250c97115a69d46cbcbcdb2a8
Lauri Helenius <a1402989@myy.haaga-helia.fi>
2016-10-31 14:16:00 -0700

    Spacing

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,14 @@
 class apache2-index {
-package { apache2:
-ensure => “installed”,
-}
+	package { apache2:
+		ensure => installed,
+		allowcdrom => true,
+	}
+
 file { “/var/www/html/index.html”:
-content => template(“apache2-index/templates/index.html”),
-}
+	content => template(“apache2-index/templates/index.html”),
+	}
+
 service { “apache2”:
-ensure => “running”,
-}
+	ensure => “running”,
+	}
 }
